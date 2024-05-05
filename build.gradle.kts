f3dc0beecc30861450f751ee0c9c21144badd2e6
Magnus Bull <magnus@norkred.no>
2024-05-05 20:06:02 +0200

    refactor: set up gradle structure

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+// I just hardcoded this for now.
+// Change to appropriate natives for the platform.
+// E.g. "natives-windows", "natives-linux-arm32"
+// Might automate that later
+val lwjglNatives = "natives-macos-arm64"
+
+plugins {
+    id("java-library")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.lwjgl:lwjgl-bom:3.3.3"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    testImplementation("org.lwjgl", "lwjgl-glfw")
+
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+}
