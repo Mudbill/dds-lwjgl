b80d7db96b8cb455d44cadd5c54a982f62d4f9d0
Magnus Bull <magnus@norkred.no>
2024-05-07 15:11:42 +0200

    build: add signing config and update build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,19 +4,64 @@
 // Might automate that later
 val lwjglNatives = "natives-macos-arm64"
 
-group = "net.buttology.lwjgl.dds"
+group = "io.github.mudbill"
 version = "2.1.1"
 
 plugins {
     id("java-library")
+    id("maven-publish")
+    id("signing")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("java") {
+            from(components["java"])
+
+            pom {
+                name = "DDS for LWJGL"
+                description =
+                    "dds-lwjgl is a tiny library for parsing DirectDraw Surface texture files for use in LWJGL's OpenGL in Java."
+                url = "https://github.com/Mudbill/dds-lwjgl"
+                inceptionYear = "2018"
+
+                licenses {
+                    license {
+                        name = "GPL-3.0"
+                        url = "https://www.gnu.org/licenses/gpl-3.0.en.html"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "mudbill"
+                        name = "Magnus Bull"
+                        email = "mudbill@buttology.net"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:git:github.com/Mudbill/dds-lwjgl.git"
+                    developerConnection = "scm:git:ssh://github.com/Mudbill/dds-lwjgl.git"
+                    url = "https://github.com/Mudbill/dds-lwjgl"
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["java"])
 }
 
 repositories {
     mavenCentral()
 }
 
+val lwjglVersion = "3.3.3"
+
 dependencies {
-    implementation(platform("org.lwjgl:lwjgl-bom:3.3.3"))
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
 
     implementation("org.lwjgl", "lwjgl")
     implementation("org.lwjgl", "lwjgl-opengl")
@@ -25,4 +70,9 @@ dependencies {
     runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
